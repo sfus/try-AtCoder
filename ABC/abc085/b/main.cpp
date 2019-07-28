8d90c405b020fbae216a9c5f7bebce6980bf5baa
sfus <sfus.dev@gmail.com>
2019-07-28 09:50:32 +0900

    Add abc085/b (Kagami Mochi)

---
+++ ABC/abc085/b/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    const int MAX = 100;
+    int bucket[MAX + 1] = {}; // for max bucket[100]
+
+    int N; cin >> N;
+    for (int i = 0; i < N; ++i) {
+        int digit;
+        cin >> digit;
+        bucket[digit] = 1; // 1 <= digit <= 100
+    }
+
+    int ret = 0;
+    for (int i = 1; i <= MAX; ++i) { // bucket[0] is not used.
+        if (bucket[i] == 1) ++ret;
+    }
+
+    cout << ret << endl;
+    return 0;
+}
