d6fe14187c8fb9169d79b4fe7fbe6c62f8d48e4f
sfus <sfus.dev@gmail.com>
2020-01-22 13:43:11 +0900

    Update abc049/c (Daydream) written in Python

--- ABC/abc049/c/main.py
+++ ABC/abc049/c/main.py
@@ -1,21 +1,23 @@
 #!/usr/bin/env python
 
-s = input()
-rev_s = s[::-1]
-words = ['dream', 'dreamer', 'erase', 'eraser']
-rev_words = [i[::-1] for i in words]
+def solve():
+    s = input()
+    rev_s = s[::-1]
+    words = ['dream', 'dreamer', 'erase', 'eraser']
+    rev_words = [i[::-1] for i in words]
 
-i = 0
-max = len(s)
-while i < max:
-    found = False
-    for word in rev_words:
-        if rev_s[i:].startswith(word):
-            found = True
-            i += len(word)
-            break
-    if not found:
-        print("NO")
-        exit()
+    i = 0
+    max = len(s)
+    while i < max:
+        found = False
+        for word in rev_words:
+            if rev_s[i:].startswith(word):
+                found = True
+                i += len(word)
+                break
+        if not found:
+            return "NO"
 
-print("YES")
+    return "YES"
+
+print(solve())
